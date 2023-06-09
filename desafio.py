7564bc833cd1b58581aa5ca07f7829ca13ec8b31
Mateus Barros <mateusverissimo94@gmail.com>
2023-06-09 15:15:54 -0300

    deposito e saque finalizados

--- desafio.py
+++ desafio.py
@@ -4,7 +4,7 @@ menu = """
 [e] Extrato
 [q] Sair
 
-"""
+ => """
 
 saldo = 0
 limite = 500
@@ -16,10 +16,35 @@ while True:
     opcao = input(menu)
 
     if opcao == 'd':
-        print("Deposito")
+        deposito = float(input('Digite o valor a ser depositado: '))
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Depósito: R$ {deposito:.2f}\n"
+        else:
+            print("Operação falhou! O valor informado é inválido")
 
     elif opcao == 's':
-        print("Sacar")
+        sacar = float(input("Informe o valor do saque: "))
+        excedeu_saldo = sacar > saldo
+        excedeu_limite = sacar > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! o valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif sacar > 0:
+            saldo -= sacar
+            extrato += f"Saque: R$ {sacar:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido")
 
     elif opcao == 'e':
         print("Extrato")
